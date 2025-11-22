23891503447a544db683b3b5acff69d76b486529
LANDOSOUZA <lando-souza@hotmail.com>
2025-11-22 20:41:50 -0300

    Recupera arquivos apagados por engano

---
+++ backend/clp/constants/status.js
@@ -0,0 +1,9 @@
+// backend/clp/constants/status.js
+const STATUS = {
+  PENDENTE: 'pendente',
+  EM_PRODUCAO: 'em_producao',
+  FINALIZADO: 'finalizado',
+  CANCELADO: 'cancelado'
+}
+
+module.exports = { STATUS }
